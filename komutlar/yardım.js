2aa1433a36ec8c33f65526e8678ed6c5be82aa4a
Glitch (discordjsv12basitaltyapi) <none>
2020-10-24 18:19:14 +0000

    🦁📟 Checkpoint ./ayarlar.json:9871782/100 ./komutlar/yardım.js:9871782/346

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,10 +6,10 @@ exports.run = async (client, message, args) => {
 
 const yardım = new Discord.MessageEmbed()
 .setColor("BLUE")
-.setAuthor("Destiny v12 sürümüyle yeniden sizlerle.")
-.setTitle("<a:Yldz:742698148329291826> Destiny'in yardım menüsüne hoşgeldin! <a:Yldz:742698148329291826>")
+.setAuthor("» CAPTAN v12 sürümüyle yeniden sizlerle.")
+.setTitle("<a:dizzy:769624534658580607> » CAPTAN'in yardım menüsüne hoşgeldin! <a:dizzy:7769624534658580607>")
  .setTimestamp()
-.setDescription("<a:destinyhype:751090021859196968> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:ayarlardestiny:751089592245158030> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:partnerdestiny:751089594694631465> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:Yldz:742698148329291826> **-logo** = __Logo komutlarını görüntülersiniz.__")
+.setDescription("<a:Alev:769625885908140063> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:eyes:769625661241294928> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:baby:769625459901071443> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:low_brightness:769624735235440650> **-logo** = __Logo komutlarını görüntülersiniz.__")
 .setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
 message.channel.send(yardım)
 }
