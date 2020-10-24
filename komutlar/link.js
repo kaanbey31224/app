9faffb4ab1e9ee607fb6457ed0fe2d2bd7b98828
Glitch (discordjsv12basitaltyapi) <none>
2020-10-24 18:42:36 +0000

    🤘🎥 Checkpoint ./komutlar/moderasyon.js:9871782/429 ./komutlar/sa-as.js:9871782/4 ./komutlar/unban.js:9871782/3 ./komutlar/yavaşmod.js:9871782/2 ./komutlar/link.js:9871782/8

--- komutlar/link.js
+++ komutlar/link.js
@@ -4,8 +4,8 @@ const db = require('quick.db');
 exports.run = async(client, message, args) => {
 
   if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(` Bu komudu kullanabilmek için "Sunucuyu Yönet" yetkisine sahip olman gerek.`)
-  if (!args[0]) return message.channel.send(`:no_entry: Reklam Filtresini Ayarlamak İçin \`!link aç\` | Kapatmak İstiyorsanız \`!link kapat\` Yazabilirsiniz`)
-  if (args[0] !== 'aç' && args[0] !== 'kapat') return message.channel.send(`:no_entry: Reklam Filtresini Ayarlamak İçin \`!link aç\` | Kapatmak İstiyorsanız \`!link kapat\` Yazabilirsiniz`)
+  if (!args[0]) return message.channel.send(`:no_entry: Reklam Filtresini Ayarlamak İçin \`-link aç\` | Kapatmak İstiyorsanız \`-link kapat\` Yazabilirsiniz`)
+  if (args[0] !== 'aç' && args[0] !== 'kapat') return message.channel.send(`:no_entry: Reklam Filtresini Ayarlamak İçin \`-link aç\` | Kapatmak İstiyorsanız \`-link kapat\` Yazabilirsiniz`)
 
     if (args[0] == 'aç') {
     db.set(`reklamFiltre_${message.guild.id}`, 'acik')
