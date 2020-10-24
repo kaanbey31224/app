fd8d95d87242b87f7093a6a5d5f78af695f2bc74
Glitch (discordjsv12basitaltyapi) <none>
2020-10-24 18:31:10 +0000

    🚒📈 Checkpoint ./komutlar/yardım.js:9871782/238 ./komutlar/eğlence.js:9871782/485 ./komutlar/moderasyon.js:9871782/189

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -6,11 +6,10 @@ exports.run = async (client, message, args) => {
 
 const eğlence = new Discord.MessageEmbed()
 .setColor("RANDOM")
-.setAuthor("Destiny v12 sürümüyle yeniden sizlerle.")
-.setTitle("<a:Yldz:742698148329291826> Destiny Eğlence Komutları <a:Yldz:742698148329291826>")
+.setAuthor("» CAPTAN v12 sürümüyle yeniden sizlerle.")
+.setTitle("<a:maple_leaf:742698148329291826> » CAPTANEğlence Komutları <a:maple_leaf:742698148329291826>")
  .setTimestamp()
-.setDescription("<a:yan:750065080741134407> **-tersyazı** = Bir Yazıyı Bot Ters Yazar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-mcskin** = Yazdığınız ismin minecraft görünüşünü atar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-fbi** = Bot FBi Gif Atar. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-token** = Tokenimi Öğrenmek İstemezmisin? <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-düello** = Düello Atarsın. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-wasted** = Polis tarafından yakalanırsın. <a:developeremoji:749525084586115153> \n <a:yan:750065080741134407> **-atatürk** = Dene ve gör... (1881-1938) <a:developeremoji:749525084586115153>")
-.setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
+.setDescription("<a:dizzy:750065080741134407> **-tersyazı** = Bir Yazıyı Bot Ters Yazar. <a:confetti_ball:749525084586115153> \n <a:dizzy:750065080741134407> **-mcskin** = Yazdığınız ismin minecraft görünüşünü atar. <a:confetti_ball:749525084586115153> \n <a:dizzy:750065080741134407> **-fbi** = Bot FBi Gif Atar. <a:confetti_ball:749525084586115153> \n <a:dizzy:750065080741134407> **-token** = Tokenimi Öğrenmek İstemezmisin? <a:confetti_ball:749525084586115153> \n <a:dizzy:750065080741134407> **-düello** = Düello Atarsın. <a:confetti_ball:749525084586115153> \n <a:dizzy:750065080741134407> **-wasted** = Polis tarafından yakalanırsın. <a:confetti_ball:749525084586115153> \n <a:dizzy:750065080741134407> **-atatürk** = Dene ve gör... (1881-1938) <a:confetti_ball:749525084586115153>")
 message.channel.send(eğlence)
 }
 
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,8 +9,7 @@ const yardım = new Discord.MessageEmbed()
 .setAuthor("» CAPTAN v12 sürümüyle yeniden sizlerle.")
 .setTitle("<a:dizzy:769624534658580607> » CAPTAN'in yardım menüsüne hoşgeldin! <a:dizzy:7769624534658580607>")
  .setTimestamp()
-.setDescription("<a:Alev:769625885908140063> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:eyes:769625661241294928> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:baby:769625459901071443> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:low_brightness:769624735235440650> **-logo** = __Logo komutlarını görüntülersiniz.__")
-.setImage("https://cdn.discordapp.com/attachments/744536225230094360/751100899836821534/Night_Sky_Mountain_Decorative_Border_Youtube_Thumbnail.png")
+.setDescription("<a:confetti_ball:769626435165749268> **-eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:eyes:769625661241294928> **-moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:baby:769625459901071443> **-kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:low_brightness:769624735235440650> **-logo** = __Logo komutlarını görüntülersiniz.__")
 message.channel.send(yardım)
 }
 
