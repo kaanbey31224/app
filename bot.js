08b65679e0de3759e6c46bab66ebd7d84f7b3344
Glitch (son-guzell) <none>
2020-11-03 20:33:30 +0000

    🎋💅 Checkpoint ./bot.js:24426515/2664

--- bot.js
+++ bot.js
@@ -701,3 +701,41 @@ client.on("guildMemberRemove", async member => {
   }
 });
 
+//------------------------ Eklendim Atıldım -----------------------//
+//------------------------ Eklendim Atıldım -----------------------//
+
+client.on('guildDelete', guild => {
+
+let rrrsembed = new Discord.RichEmbed()
+
+.setColor("RED")
+.setTitle("Bot Kicklendi")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucu sahibi", guild.owner)
+.addField("Sunucu Sahibi'nin İd'si", guild.ownerID)
+.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.get('754730560009797723').send(rrrsembed);
+  
+});
+
+client.on('guildCreate', guild => {
+
+let rrrsembed = new Discord.RichEmbed()
+
+.setColor("GREEN")
+.setTitle("Bot Eklendi")
+.addField("Sunucu Adı:", guild.name)
+.addField("Sunucu sahibi", guild.owner)
+.addField("Sunucu Sahibi'nin İd'si", guild.ownerID)
+.addField("Sunucunun Kurulu Olduğu Bölge:", guild.region)
+.addField("Sunucudaki Kişi Sayısı:", guild.memberCount)
+
+   client.channels.get('754730560009797723').send(rrrsembed);
+  
+});
+
+//------------------------ Eklendim Atıldım -----------------------//
+//------------------------ Eklendim Atıldım -----------------------//
+//------------------------ Eklendim Atıldım -----------------------//
