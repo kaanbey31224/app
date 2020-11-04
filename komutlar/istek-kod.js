968622a2f1564e01d44dbe178b093c8758ff7aa1
Glitch (son-guzell) <none>
2020-11-04 10:31:04 +0000

    📻🖖 Checkpoint ./komutlar/istek-kod.js:24426515/23

--- komutlar/istek-kod.js
+++ komutlar/istek-kod.js
@@ -11,7 +11,7 @@ const embed = new Discord.MessageEmbed()
 .setDescription(`**istek Kanalı** ${message.channel.name} \n **istek Bildirilen Sunucu** \`${message.guild.name}\` \n **İstek Bildiren Kullanıcı** <@${message.author.id}> \n **İstenen komut :** \`${istek}\``)
 client.channels.cache.get('772082804204896266').send(embed)
   
-message.channel.send("Bug bildiriminiz gönderildi. :confetti_ball:").then(message => message.delete({ timeout: 5000 }));
+message.channel.send("İstek kod  bildiriminiz gönderildi. :confetti_ball:").then(message => message.delete({ timeout: 5000 }));
 };
 exports.conf = {
   enabled: true,
