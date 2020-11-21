281cd99d20f0a62cf1cf53e94f3fcf0c664a3532
Glitch (son-guzell) <none>
2020-11-21 16:00:03 +0000

    🌗⭐️ Checkpoint ./komutlar/eğlece.js:24426515/3283 ./komutlar/eğlence.js:24426515/432 ./komutlar/eğlence2.js:24426515/290

--- komutlar/eğlece.js
+++ komutlar/eğlece.js
@@ -8,37 +8,7 @@ const EmbedPinCode = new Discord.MessageEmbed()
 .setTitle("**▬▬▬▬▬▬[** ©️ **» CAPTAN Bot** ©️ **]▬▬▬▬▬▬**")
 .setThumbnail("https://cdn.discordapp.com/emojis/770265448892858368.gif?v=1")
 .setDescription(`
-**» Bağlantılar** 
-**[Destek Sunucusu](https://discord.gg/qzHTkhjQgT)** **•** **[Botun Davet Linki](https://discord.com/api/oauth2/authorize?client_id=779006421625995345&permissions=8&scope=bot)** **•** **[Web-Site]( https://hammerhead-spark-sheet.glitch.me/index.html)**
-Bir komut hakkında detaylı __yardım için__: **-yardım**
 
-• Bot ile ilgili tavsiyelerinizi veya bulduğunuz hataları **-istek-kod** yazarak bize iletebilirsiniz.
-• Hatalı bi kod veya buglu bi kod bulursanız **-bug-bildir** yazarak bize bildirebilirsiniz.
-
-**• Komutlar**
-> [-zarat](https://discord.gg/qzHTkhjQgT) →  Zar atarsınız
-> [-kasaaç](https://discord.gg/qzHTkhjQgT) → CsGo kasası açmaya ne dersin?.
-> [-emojiyazı](https://discord.gg/qzHTkhjQgT) → Emojili yazı yazmaya ne dersin?.
-> [-espiri](https://discord.gg/qzHTkhjQgT) → Bot espiri yapar.
-> [-sor](https://discord.gg/qzHTkhjQgT) → Bot sorduğunuz soruya cevap verir.
-> [-ara112](https://discord.gg/qzHTkhjQgT) → Ambulansı ararsınız.
-> [-vine](https://discord.gg/qzHTkhjQgT) → Bot size komik paylaşımlar gösterir.
-> [-adamasmaca](https://discord.gg/qzHTkhjQgT) → Adam asmaca oyunu.
-> [-balıktut](https://discord.gg/qzHTkhjQgT) → Dereden balık tutarsınız.
-> [-tersyazı](https://discord.gg/qzHTkhjQgT) →  Yazdığınız yazıyı bot tersten yazar
-> [-mcskin](https://discord.gg/qzHTkhjQgT) →Yazdığınız ismin minecraft görünüşünü atar.
-> [-fbi](https://discord.gg/qzHTkhjQgT) → FBİ gifi atar.
-> [-token](https://discord.gg/qzHTkhjQgT) →  
-> [-](https://discord.gg/qzHTkhjQgT) →  
-> [-](https://discord.gg/qzHTkhjQgT) → 
-
-
-**▬▬▬▬▬▬▬[** :gear:  **Bilgilendirme**  :gear: **]▬▬▬▬▬▬**
-> :bulb: **Discord Js Sürümü : 12.4.0**
-> :robot: **quick.db Sürümü : 7.1.2**
-> :eye_in_speech_bubble: **moment Sürümü : 2.29.1**
-© 2020 Captan | Tüm hakları saklıdır.
-`)
  
 
 return message.channel.send(EmbedPinCode)
@@ -53,7 +23,7 @@ exports.conf = {
 };
   
   exports.help = {
-    name: 'eğlencea', 
+    name: 'eğlence', 
     description: 'Botun Komut Listesini Gösterir!',
     usage: '-eğlence'
 };
