a3d3c2f4c2a37c35c5f91edd7ead59e5e27ace0c
Glitch (son-guzell) <none>
2020-11-26 08:22:20 +0000

    🎲🔔 Checkpoint ./komutlar/yardım.js:24426515/220

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -22,10 +22,11 @@ Bir komut hakkında detaylı __yardım için__: **-yardım**
 > [-logo](https://discord.gg/qzHTkhjQgT) → Logo komutları.
 > [-gifyardım](https://discord.gg/qzHTkhjQgT) → Gif komutları.
 
-**▬▬▬▬▬▬▬[** :gear:  **Bilgilendirme**  :gear: **]▬▬▬▬▬▬**
+**• Bilgilendirme**
+
 > :bulb: **Discord Js Sürümü : 12.4.0**
-> :robot: **quick.db Sürümü : 7.1.2**
-> :eye_in_speech_bubble: **moment Sürümü : 2.29.1**
+> :robot: **Yapımcım** Manyak*#9999
+> :eye_in_speech_bubble: **Sürümüm : ** 2.0.1
 © 2020 Captan | Tüm hakları saklıdır.
 `)
  
