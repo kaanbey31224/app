257da1afd7ad96ee1cb7ab73b85bd014a346d72e
Glitch (son-guzell) <none>
2020-11-27 14:06:40 +0000

    🐸🚪 Checkpoint ./komutlar/market.js:24426515/2

--- komutlar/market.js
+++ komutlar/market.js
@@ -3,7 +3,7 @@ const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
   
-  if (!args[0]) return message.reply(`Matket şuan kapalı malesef :name_badge: `)
+  if (!args[0]) return message.reply(`Market şuan kapalı malesef :name_badge: `)
   
   if (args[0] === 'sa') {
     let Captanpara = await db.get(`para_${message.author.id}`) 
