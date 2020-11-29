56a1b66166d67b5534c3cf80d8e978af6dbdcda4
Glitch (son-guzell) <none>
2020-11-29 15:39:30 +0000

    😅🚅 Checkpoint ./komutlar/yard2ım.js:24426515/756

--- komutlar/yard2ım.js
+++ komutlar/yard2ım.js
@@ -5,17 +5,40 @@ exports.run = (client, message) => {
 const EmbedCrewCode = new Discord.MessageEmbed()
 
 .setColor("RANDOM")
-.setTitle("**  **")
+.setTitle("****")
 .setThumbnail("https://cdn.discordapp.com/emojis/770265448892858368.gif?v=1")
 .setDescription(`
 Captan Ana Menüsüne Hoş Geldin Dostum 
-@779006421625995345 Kullanırken `` @Captan `` rölünü en yukarıda tutunuz.
+» CAPTAN#7332 Kullanırken » CAPTAN rölünü en yukarıda tutunuz.
 
 
-``-kayıt-icerik``
+**-kayıt-icerik**
 
->Kayıt,Kutulu ``&`` Kutusuz,Diğer,Tag rol,
-> Koruma,Say,Aktiflik,Mesaj tag
+> Kayıt,Kutulu **&** Kutusuz,Diğer,Tag rol,
+> Koruma,Say,Aktiflik,Mesaj tag.
+
+**-seviye-sistem**
+
+> Basit Seviye,Seviye sırala **&** Büyük 
+> depolama alan.
+
+**yardım2** **Menüsün de neler var?**
+
+> Mut,KarantinaiBan,Tolu rol ,Sayaç **&** Oto
+> rol,Kısıtlamalar.
+
+**-yardım3** **Menüsün de nerler var?
+
+> Yasak tag,Oto isin,Toplu mesaj,Fake hesap,
+> Sunucu tema,Kanal arıdır **&** Rol arıdır.
+
+
+**İninal Barkod NO:** **123456789123**
+**Papara Hesap NO:** **1234567891**
+
+
+
+
 `)
  
 
