80d9fcac904bd1a0f73c4dcf58821f418bd3295c
Glitch (captan-bot-v2) <none>
2020-11-30 09:08:53 +0000

    ☕️🍎 Checkpoint ./komutlar/boatbilgi.js:24426515/3024

--- komutlar/boatbilgi.js
+++ komutlar/boatbilgi.js
@@ -4,17 +4,20 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
 
   
-const istatistikler = new Discord.MessageEmbed()
-  .addField(`:desktop:** İstatistikler**`,)
+const Crewistatistikler = new Discord.MessageEmbed()
+  .addField(`:desktop: **İstatistikler**`,)
   .addField(`:traffic_light: »  Pingim` ,`${client.ws.ping}ms`,true)
   .addField(`:woman_technologist: » Yapımcım :registered: ` ,`<@754730560009797723>`,true)
   .addField(`:globe_with_meridians: » Node.js`, `${process.version}`, true)
-.addField(`<a:reminder_ribbon:742698007262396426> Kütüphanesi;`, `Discord.js`, true)
  .addField(`:bricks: » Kanal Sayısı`, `${client.channels.cache.size  }`, true)
  .addField(`:family_man_woman_boy: » Kullanıcı Sayısı`, `${client.users.cache.size}`, true)
  .addField(`:envelope: » Sunucu Sayısı`, `${client.guilds.cache.size}`, true)
+ .addField('<a:reminder_ribbon:742698007262396426> Bellek kullanımı:', (process.memoryUsage().heapUsed / 2048 / 2048).toFixed(2))
+.addField(`<a:reminder_ribbon:742698007262396426> Kütüphanesi;`, `Discord.js`, true)
+.addField(`<a:reminder_ribbon:742698007262396426> Discord.js sürümü:`, Discord.version)
+  .addField(' Botun Yapılmaya Başlandığı Tarih ', '**15.10.2020**')
  .addField(`» Linkler`, `[Destek Sunucusu](https://discord.gg) **|** [YouTube](https://discord.gg)`, true)
-  message.channel.send(istatistikler)
+  message.channel.send(Crewistatistikler)
   
   
 }
