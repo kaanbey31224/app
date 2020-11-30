553ac691e50009f19caeee38d3a4d4a24663659f
Glitch (captan-bot-v2) <none>
2020-11-30 08:57:42 +0000

    💴💖 Checkpoint ./komutlar/boatbilgi.js:24426515/418

--- komutlar/boatbilgi.js
+++ komutlar/boatbilgi.js
@@ -6,13 +6,14 @@ exports.run = async (client, message, args) => {
   
 const istatistikler = new Discord.MessageEmbed()
   .addField(`:desktop:** İstatistikler**`,)
-  .addField(`:scroll: »  Pingim` ,`${client.ws.ping}ms`,true)
+  .addField(`:traffic_light: »  Pingim` ,`${client.ws.ping}ms`,true)
   .addField(`:woman_technologist: » Yapımcım :registered: ` ,`<@754730560009797723>`,true)
-  .addField(`:label: » Node.js`, `${process.version}`, true)
- .addField(`:bar_chart: » Kanal Sayısı`, `${client.channels.cache.size  }`, true)
+  .addField(`:globe_with_meridians: » Node.js`, `${process.version}`, true)
+.addField(`<a:reminder_ribbon:742698007262396426> Kütüphanesi;`, `Discord.js`, true)
+ .addField(`:bricks: » Kanal Sayısı`, `${client.channels.cache.size  }`, true)
  .addField(`:family_man_woman_boy: » Kullanıcı Sayısı`, `${client.users.cache.size}`, true)
  .addField(`:envelope: » Sunucu Sayısı`, `${client.guilds.cache.size}`, true)
- .addField(`» Linkler`, `[Destek Sunucusu](https://discord.gg) | [Youtube](https://www.youtube.com/c/ukqzn`, true)
+ .addField(`» Linkler`, `[Destek Sunucusu](https://discord.gg) **|** [YouTube](https://discord.gg)`, true)
   message.channel.send(istatistikler)
   
   
