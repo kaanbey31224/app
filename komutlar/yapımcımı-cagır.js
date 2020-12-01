68b9b056da7bba37c7818a1ffbf0f8732291b558
Glitch (captan-bot-v2) <none>
2020-12-01 19:57:54 +0000

    🗻🏘 Checkpoint ./komutlar/yapımcımı-cagır.js:24426515/1797

---
+++ komutlar/yapımcımı-cagır.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js");
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, args) => {
+   let Crew = args.slice(0).join(' ');
+   
+  if (!Crew) return message.reply(':x:Sahibi neden çağıracağım!')
+  if (Crew.length < 6) return message.channel.send(":x: Sebebiniz Daha Geçerli Olması İçin **En Az 6** Karakterli Olmalıdır!");
+  if (!message.member.hasPermission("CREATE_INSTANT_INVITE")) return;
+  message.channel.createInvite({maxAge: 0}).then(CrewCode => {
+
+    message.channel.send("Sahibime Mesajını İlettim!")
+    let Manyak = new Discord.MessageEmbed()
+    .setColor('RED')
+    .setAuthor(`${message.author.tag} (${message.author.id})`, `${message.author.avatarURL}`)
+    .setTitle('Biri Seni Yardın İçin Çağırdı')
+    .addField('Sebep:', `${Manyak}`)
+    .addField(`Sunucunun Davet Linki:`, `${CrewCode}`)
+    .setThumbnail(message.guild.iconURL)
+    .setFooter('Crew CODE');
+    client.users.get(ayarlar.sahip).send(Crew);
+  });
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["y-i"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ilet',
+  description: '',
+  usage: 'ilet'
+}; 
