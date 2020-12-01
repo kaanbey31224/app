01d14bb49b71e645284fb03662baae528553831d
Glitch (captan-bot-v2) <none>
2020-12-01 20:21:18 +0000

    ⛑🏋 Checkpoint ./ayarlar.json:24426515/10 ./img/wasted/manyak.png:24426515/108 ./komutlar/duyuru.js:24426515/70

--- komutlar/duyuru.js
+++ komutlar/duyuru.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
   let mesaj = args.slice(0).join(' ');
-if (mesaj.length < 1) return message.reply('Lütfen Duyuru Metnini Giriniz.');
+if (mesaj.length < 1) return message.reply('Yazacağım metni yazarsan sevinirim');
   message.delete();
   message.channel.send(mesaj);
 };
