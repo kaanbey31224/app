9f8b0a953c9a77e6b383ff01da2e8ec8f91ac6fa
Glitch (captan-bot-v2) <none>
2020-12-02 15:43:14 +0000

    🛬☎️ Checkpoint ./komutlar/rol-ver.js:24426515/1815 ./komutlar/extra.js:24426515/1593

---
+++ komutlar/rol-ver.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+exports.run = (client, message, args) => {
+  if (!message.guild) {
+    const CrewCode = new Discord.MessageEmbed()
+      .setColor(0xff0000)
+      .setTimestamp()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .addField(
+        "⚠ **Uyarı** ⚠",
+        "`rol-ver` **Adlı Komutu Özel Mesajlarda Kullanamazsın!**"
+      );
+    return message.author.sendEmbed(CrewCode);
+  }
+  if (!message.member.hasPermission("MANAGE_ROLES"))
+    return message.reply(
+      "❌ Bu Komutu Kullana Bilmek için `Rolleri Yönet` Yetkisine Sahip Olmalısın!"
+    );
+  let guild = message.guild;
+  let rol = message.mentions.roles.first();
+  let user = message.mentions.members.first();
+
+  if (!user)
+    return message
+      .reply("**⚠ Rol verebilmek icin kullanıcının ismini ve verilecek rölü yazmalısın! ⚠ **")
+      .catch(console.error);
+
+  user.roles.add(rol);
+  const CrewCode = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setDescription(
+      `✅  Başarıyla ${user} İsimli Kullanıcıya ${rol} İsimli Rol Verildi!`
+    )
+    .setFooter("Captan Bot");
+  message.channel.send(CrewCode);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [""],
+  permLevel: 0
+};
+ //Crew Code
+exports.help = {
+  name: "rol-ver",
+  description: "İstediğiniz kişiyi istediğiniz rolü verir.",
+  usage: "rol-ver [kullanıcı] [@rol]"
+};
