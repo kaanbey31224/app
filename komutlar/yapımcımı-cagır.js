1881d7de17c9b2b5e155de7e0074cdc36e12d521
Glitch (captan-bot-v2) <none>
2020-12-02 10:58:09 +0000

    🏇💖 Checkpoint ./komutlar/yapımcımı-cagır.js:24426515/187

--- komutlar/yapımcımı-cagır.js
+++ komutlar/yapımcımı-cagır.js
@@ -5,16 +5,16 @@ exports.run = (client, message, args) => {
    let Crew = args.slice(0).join(' ');
    
   if (!Crew) return message.reply(':x:Sahibi neden çağıracağım!')
-  if (Crew.length < 6) return message.channel.send(":x: Sebebiniz Daha Geçerli Olması İçin **En Az 6** Karakterli Olmalıdır!");
+  if (Crew.length < 6) return message.channel.send(":x: Karakter sayısı cok az tekrar deneyiniz. **En az 10 karakterli olmalıdır!**");
   if (!message.member.hasPermission("CREATE_INSTANT_INVITE")) return;
   message.channel.createInvite({maxAge: 0}).then(CrewCode => {
 
     message.channel.send("Sahibime Mesajını İlettim!")
-    let Manyak = new Discord.MessageEmbed()
+    let manyak = new Discord.MessageEmbed()
     .setColor('RED')
     .setAuthor(`${message.author.tag} (${message.author.id})`, `${message.author.avatarURL}`)
     .setTitle('Biri Seni Yardın İçin Çağırdı')
-    .addField('Sebep:', `${Manyak}`)
+    .addField('Sebep:', `${manyak}`)
     .addField(`Sunucunun Davet Linki:`, `${CrewCode}`)
     .setThumbnail(message.guild.iconURL)
     .setFooter('Crew CODE');
